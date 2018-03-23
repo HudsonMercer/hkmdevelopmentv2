783cc76ca0dba84daf79bea781beb9c4b5b2b121
Brandon Grissom <lousaydus@gmail.com>
2018-03-23 16:10:50 -0700

    updated home page to give a small hit as to what the user should do.

--- src/components/HomePane.jsx
+++ src/components/HomePane.jsx
@@ -24,17 +24,8 @@ export default class HomePane extends Component {
 
   render() {
     return ( <div className="homePane">
-      <div className="paneContent">
-        <PaneContent className="paneContentHomeColor" headerText={this.getTitle()}>
-          <div>
-            Phone: 208-964-4410
-            <br/>
-            Email @ brandon.grissom89@gmail.com
-            <br/>
-            Over the top? over the top.... take it down a notch....
-          </div>
-        </PaneContent>
-      </div>
+      <h1>Look over there!</h1>
+      <div className="paneHomeArrow"></div>
     </div> )
   }
 }
